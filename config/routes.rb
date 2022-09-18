f941f9193dca123868bbd2aed99610b585f2fe6e
FujisawaAkifumi <112544215+fujisawa1234@users.noreply.github.com>
2022-09-18 11:22:38 +0000

    [start]

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  devise_for :users
+  resources :users, only: [:index, :show, :edit]
+  resources :books, only: [:new, :index, :show]
+  root to: "homes#top"
+  get 'homes/about'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end

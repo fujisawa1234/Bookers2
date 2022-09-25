80b974ce1f25778fed3efa6376aa8f3db137e92a
FujisawaAkifumi <112544215+fujisawa1234@users.noreply.github.com>
2022-09-25 08:19:05 +0000

    [finish]エラー修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
   end
 
   def show
-     @user = User.find(params[:id])
+    @user = User.find(params[:id])
     @books = @user.books
     @book = Book.new
   end
